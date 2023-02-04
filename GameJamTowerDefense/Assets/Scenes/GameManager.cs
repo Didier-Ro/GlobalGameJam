a3279249f3802e85dfac412840cfc3be06adee02
JesusRuiz02 <79652924+JesusRuiz02@users.noreply.github.com>
2023-02-04 01:08:27 -0700

    aaa

--- GameJamTowerDefense/Assets/Scenes/GameManager.cs
+++ GameJamTowerDefense/Assets/Scenes/GameManager.cs
@@ -1,4 +1,3 @@
-using System;
 using TMPro;
 using UnityEngine;
 
@@ -6,38 +5,61 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private float _timeleft;
     [SerializeField] private float _time;
-    private bool _onPause = false;
-    private bool _defenseTurn = default;
+    [SerializeField] private int _roundLeft = default;
+    [SerializeField] private bool _timerOn = false;
+    [SerializeField] private bool _defenseTurn = default;
     [SerializeField] private TextMeshProUGUI textTime = default;
     private MenuData _menuData;
 
     private void Start()
     {
         _menuData = GetComponent<MenuData>();
-        _time = _menuData.GetComponent<MenuData>().PreparationTime;
-        DefenseTurn();
+        _timeleft = _menuData.PreparationTime;
+        _time = _menuData.PreparationTime;
+        _roundLeft = _menuData.Rounds;
+        _defenseTurn = true;
+        _timerOn = false;
     }
 
     void Update()
     {
-        if(!_onPause)
+        if(_timerOn)
         {
-            _time -= Time.deltaTime;
-            textTime.text = _time.ToString("0");
-            if (_timeleft <= 0)
+            if (_timeleft > 0)
             {
-              AttackTurn();  
+                _timeleft -= Time.deltaTime;
             }
+            else
+            {
+                _timeleft = _menuData.PreparationTime;
+                _timerOn = false;
+                if (!_defenseTurn)
+                {
+                    _roundLeft--;
+                }
+                _defenseTurn = !_defenseTurn;
+            }
+            textTime.text = _timeleft.ToString("0");
+        }
+        else
+        {
+            PreparationTime();
+            textTime.text = _timeleft.ToString("0");
         }
     }
 
-    private void DefenseTurn()
+    private void PreparationTime()
     {
-        _defenseTurn = true;
+        if (_timeleft > 0)
+        {
+            _timeleft -= Time.deltaTime;
+        }
+        else
+        {
+            _timeleft = _menuData.RoundDuration;
+            _timerOn = true;
+        }
+        
     }
 
-    private void AttackTurn()
-    {
-        _defenseTurn = false;
-    }
 }
--- GameJamTowerDefense/Assets/Scenes/MenuData.cs
+++ GameJamTowerDefense/Assets/Scenes/MenuData.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class MenuData : MonoBehaviour
 {
     [SerializeField] private float _preparationTime = 15f;
+    [SerializeField] private float _roundDuration = 60f;
     [SerializeField] private int _rounds = 3;
 
-    public float Rounds => _rounds;
+    public int Rounds => _rounds;
+    public float RoundDuration => _roundDuration;
     public float PreparationTime => _preparationTime;
     
 }
